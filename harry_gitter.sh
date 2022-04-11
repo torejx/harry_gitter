62f8f3526144f7a7ad9d7dcce6e77f7009b6544f
salvatore <walter.dorian@gmail.com>
2022-04-11 23:25:48 +0200

    add main script

---
+++ harry_gitter.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+git()
+{
+  if [ "$1" = "protego" ] && [ "$2" = "maxima" ]
+  then
+    shift 2
+    git init "$@"
+  elif [ "$1" = "accio" ]
+  then
+    shift
+    git fetch "$@"
+  elif [ "$1" = "immobulus" ]
+  then
+    shift
+    git commit "$@"
+  elif [ "$1" = "legilimens" ] || [ "$1" = "lumos" ]
+  then
+    shift
+    git log "$@"
+  elif [ "$1" = "aguamenti" ]
+  then
+    shift
+    git add "$@"
+  elif [ "$1" = "prior" ] && [ "$2" = "incantatio" ]
+  then
+    shift 2
+    git blame "$@"
+  elif [ "$1" = "depulso" ]
+  then
+    shift
+    git push "$@"
+  elif [ "$1" = "oblivion" ]
+  then
+    shift
+    git reset "$@"
+  elif [ "$1" = "reparo" ] || [ "$1" = "reverte" ]
+  then
+    shift
+    git revert "$@"
+  elif [ "$1" = "revelio" ]
+  then
+    shift
+    git status "$@"
+  elif [ "$1" = "proteus" ]
+  then
+    shift
+    git merge "$@"
+  elif [ "$1" = "geminio" ]
+  then
+    shift
+    git clone "$@"
+  elif [ "$1" = "confundo" ]
+  then
+    shift
+    git rebase "$@"
+  elif [ "$1" = "descendo" ]
+  then
+    shift
+    git pull "$@"
+  elif [ "$1" = "dissendium" ]
+  then
+    shift
+    git checkout "$@"
+  else
+    command git "$@"
+  fi
+}
